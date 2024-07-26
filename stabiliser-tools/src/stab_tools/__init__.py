9562a357e029f20465882457f6afeeb65e8ae4ef
MrLatinNerd <minghuayin1729@gmail.com>
2024-07-25 17:17:12 -0700

    Delete venv folder

--- stabiliser-tools/src/stab_tools/__init__.py
+++ stabiliser-tools/src/stab_tools/__init__.py
@@ -1,6 +1,6 @@
 import os
 import sys
-PATH_TO_LIBRARY = f'{os.path.abspath(os.path.dirname(__file__))}/../build'
+PATH_TO_LIBRARY = f'{os.path.abspath(os.path.dirname(__file__))}/build'
 if PATH_TO_LIBRARY not in sys.path:
     # print(f'Adding {PATH_TO_LIBRARY} to path')
     sys.path.extend([PATH_TO_LIBRARY])
