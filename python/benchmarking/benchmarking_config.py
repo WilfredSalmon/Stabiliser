99dd7f2e2a01cc8bb3f04207c4562a0d1ca27bb1
WilfredSalmon <was29@cam.ac.uk>
2024-08-13 16:14:22 +0100

    changed benchmarking config

--- python/benchmarking/benchmarking_config.py
+++ python/benchmarking/benchmarking_config.py
@@ -2,12 +2,7 @@ import generators as gs
 import qiskit.quantum_info as qi
 import stim
 
-import sys
-PATH_TO_LIBRARY = './build/ninja-multi-vcpkg/cpp/src/Release'
-PATH_TO_STIM_MOCK = './build/ninja-multi-vcpkg/cpp/benchmarking/stim/Release'
-sys.path.extend([PATH_TO_LIBRARY, PATH_TO_STIM_MOCK])
 import fast as fst
-import stim_mock as sm
 
 def qiskit_C1_converter(matrix, assume_valid = True):
     return qi.Clifford.from_matrix(matrix)
